dd0c113a330a50b616ac8e73c3c8125e162ae80f
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-07-24 13:28:30 -0400

    Format Comments in Profile and Core Controllers
    Format comments to make it slightly easier to read.
    
    Also, remove tab and replace with just one space so that the code is easier to read on my vertical monitor.

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Controllers;
+//Note: The Authenticated controller is in the same namespace as Profile
 
 use \Core\View;
 use App\Auth;
 
 /* Profile Controller (Private due to inheriting from the Authenticated abstract class) */
 class Profile extends Authenticated {
-    //Note: The Authenticated controller is in the same namespace as Profile
 
     /* METHOD: index
-    *   @param void     :
-    *   @return void    :   Render the index template
+    * @param void     :
+    * @return void    :   Render the index template
     */
     public function indexAction() {
         View::renderTemplate('Profile/index.html');
