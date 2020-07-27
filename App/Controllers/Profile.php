008293cf9292473cfc531e68b98a994a9fd4c186
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-07-27 13:37:40 -0400

    Add Profile/update Action
    Add new method to Profile Controller which gets a $user object and then calls the updateUserProfile method, supplying the data from $_POST.
    
    If successful data update, then show a success message and redirect.
    
    If unsuccessful data update, then re-render Profile/edit with the $user object.

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -4,14 +4,16 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Auth;
+use \App\Flash;
 
 /* Profile Controller (Private due to inheriting from the Authenticated abstract class) */
 class Profile extends Authenticated {
 
-    /* METHOD: index
-    * @param void     :
-    * @return void    :   Render the index template
-    */
+    /**
+     * METHOD: index
+     * @param void     :
+     * @return void    : Render the index template
+     */
     public function indexAction() {
         View::renderTemplate('Profile/index.html',
             [
@@ -20,15 +22,41 @@ class Profile extends Authenticated {
     }
 
 
-    /* METHOD: edit
-    * @param void   :
-    * @return void  :
-    */
+    /**
+     * METHOD: editAction
+     * @param void   :
+     * @return void  :
+     */
     public function editAction() {
         View::renderTemplate('Profile/edit.html',
         [
             'user' => Auth::getUser()
         ]);
+    }
+
+    /**
+     * METHOD: updateAction
+     * @param void
+     * @return void : Update the user record in the database
+     */
+    public function updateAction() {
+        $user = Auth::getUser();
+
+        if ($user->updateUserProfile($_POST)) {
+            
+            // UPDATE query was successful
+            Flash::addMessage('Changes saved');
+            $this->redirect('/profile/index');
+        }
+
+        else {
+
+            // Validation Error - Redisplay the form
+            View::renderTemplate('Profile/edit.html',
+            [
+                'user' => $user
+            ]);
+        }
     }
 }
 ?>
