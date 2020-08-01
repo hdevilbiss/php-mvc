2660ea771471ae6c0cace4e3722a9c1bd56a4399
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-07-31 21:10:38 -0400

    Add Example User Class To Test
    One method tests whether the $user object returns the expected first and last name when it runs a custom method.
    
    The other method tests whether that same method returns an empty string if neither name was set.

---
+++ tests/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase {
+    /**
+     * @test
+     */
+    public function returns_full_name() {
+        require 'themes/User.php';
+        $user = new User;
+        $user->first_name = "Teresa";
+        $user->last_name = "Green";
+
+        $this->assertEquals('Teresa Green',$user->getFullName());
+    }
+
+    /**
+     * @test
+     */
+    public function full_name_is_empty_by_default() {
+        $user = new User;
+        $this->assertEquals('',$user->getFullName());
+    }
+}
+?>
