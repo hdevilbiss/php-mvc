ffca539d15ad73c02966d68a54874cefbe4183c3
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-08-04 22:02:24 -0400

    Create src classes for testing
    Create some classes in the src folder for use in PHPUnit testing.

---
+++ src/Queue.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Queue
+ * A first-in, first-out data object
+ */
+class Queue {
+    /**
+     * @var array   : Items in the queue
+     */
+    protected $items = [];
+
+    /**
+     * METHOD: push
+     * @param mixed : $item in the queue
+     * @return void : Add the $item to the $items array
+     */
+    public function push($item) {
+        $this->items[] = $item;
+    }
+
+    /**
+     * METHOD: pop
+     * @param void
+     * @return mixed    : Return the value of the last element, shortening the $items array by 1
+     */
+    public function pop() {
+        return array_pop($this->items);
+    }
+
+    /**
+     * METHOD: getCount
+     * @param void
+     * @return integer  : Quantity of $item in $items array
+    */
+    public function getCount() {
+        return count($this->items);
+    }
+}
