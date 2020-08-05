03a52f50393a8585db1c1e1c195c76df577eeeb2
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-08-04 22:13:23 -0400

    Add Methods to QueueTest
    Add two new methods to QueueTest:
    
    1. Push 1 string to the $queue and check whether the getCount method returns 1.
    
    2. Push 1 string to the $queue, then immediately pop it off, saving it as $item. Check whether $queue->getCount returns 0. Next, check whether the value of $item equals the original string value.

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -10,4 +10,29 @@ class QueueTest extends TestCase {
 
         $this->assertEquals(0,$queue->getCount());
     }
+
+    /**
+     * @test whether an $item was added to the queue
+     */
+    public function an_item_is_added_to_the_queue() {
+        $queue = new Queue;
+
+        $queue->push('green');
+        $this->assertEquals(1,$queue->getCount());
+    }
+
+    /**
+     * @test whether an $item was removed from the queue
+     */
+    public function an_item_is_removed_from_the_queue() {
+        $queue = new Queue;
+
+        $queue->push('green');
+
+        $item = $queue->pop();
+
+        $this->assertEquals(0,$queue->getCount());
+
+        $this->assertEquals('green',$item);
+    }
 }
