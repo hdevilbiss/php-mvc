4954e600b9144a24dbbc783ced565338a6ef150e
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-08-11 22:00:43 -0400

    Add test function dependency by adding an @depends <function_name> into the comment block above each test. return the value of queue at the end of a 'producer' function to pass it into the 'consumers'. The consuming functions have the  parameter type-hinting with the Queue object.

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -6,28 +6,33 @@ class QueueTest extends TestCase {
      * @test whether the count in an empty queue returns 0
      */
     public function new_queue_is_empty() {
+        //This main test is called a producer because other tests depend on it
+        
         $queue = new Queue;
 
         $this->assertEquals(0,$queue->getCount());
+
+        return $queue;
     }
 
     /**
      * @test whether an $item was added to the queue
+     * @depends new_queue_is_empty
      */
-    public function an_item_is_added_to_the_queue() {
-        $queue = new Queue;
+    public function an_item_is_added_to_the_queue(Queue $queue) {
+        // This "dependent" test is called a consumer
 
         $queue->push('green');
         $this->assertEquals(1,$queue->getCount());
+
+        return $queue;
     }
 
     /**
      * @test whether an $item was removed from the queue
+     * @depends an_item_is_added_to_the_queue
      */
-    public function an_item_is_removed_from_the_queue() {
-        $queue = new Queue;
-
-        $queue->push('green');
+    public function an_item_is_removed_from_the_queue(Queue $queue) {
 
         $item = $queue->pop();
 
