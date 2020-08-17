d5f5bc6002b7fac400efc360e2afef4c79fed872
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-08-17 12:20:03 -0400

    Update QueueTest
    Add testMaxNumberOfItemsCanBeAdded to check whether the for loop with MAX_ITEMS actually gets limited to MAX_ITEMS.
    
    Test whether trying to add too many items returns an Exception, and whether that Exception returns the expected code.

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -7,18 +7,26 @@ class QueueTest extends TestCase {
     
     /**
      * METHOD: setUp
-     * Empty the $queue before each test method
+     * This method gets run before each test method to empty the $queue before each test method
      */
     protected function setUp(): void
     {
         static::$queue->clear();        
     }
     
+    /**
+     * METHOD: setUpBeforeClass
+     * This method gets run only once, before the first test method
+     */
     public static function setUpBeforeClass(): void
     {
         static::$queue = new Queue;        
     }
     
+    /**
+     * METHOD: tearDownAfterClass
+     * This method gets run only once, after the last est method
+     */
     public static function tearDownAfterClass(): void
     {
         static::$queue = null;        
@@ -53,5 +61,38 @@ class QueueTest extends TestCase {
         static::$queue->push('second');
         
         $this->assertEquals('first', static::$queue->pop());
-    }    
+    }
+
+    /**
+     * @test whether adding the max number of $items to the $queue equals the MAX_ITEMS constant
+     */
+    public function testMaxNumberOfItemsCanBeAdded() {
+        
+        for ($i = 0; $i < Queue::MAX_ITEMS; $i++) {
+            static::$queue->push($i);
+        }
+        $this->assertEquals(Queue::MAX_ITEMS, static::$queue->getCount());
+    }
+
+    /**
+     * @test whether an Exception gets thrown when more $items than MAX_ITEMS gets added to the $queue, and whether the Exception message matches our expectation
+     */
+    public function testExceptionThrownWhenAddingItemToFullQueue() {
+        for ($i = 0; $i < Queue::MAX_ITEMS; $i++) {
+            static::$queue->push($i);
+        }
+        
+        /**
+         * PHPUnit has a few built-in methods to deal with expecting Exceptions:
+         * expectException
+         * expectExceptionCode
+         * expectExceptionMessage
+         * expectExceptionMessageRegExp
+         */
+        $this->expectException(QueueException::class);
+        
+        $this->expectExceptionMessage("Queue is full");
+
+        static::$queue->push("Broke the camel's back");
+    }
 }
