7634275897f948d7d09c435246e954dbdbf2b57d
shed-00 <45151652+shed-00@users.noreply.github.com>
2020-09-08 17:07:51 -0400

    Add check to sendMessage
    Add check to send message which throws an Exception if the  is empty

--- src/Mailer.php
+++ src/Mailer.php
@@ -12,6 +12,11 @@ class Mailer {
      * @return boolean  : True if sent
      */
     public function sendMessage($email,$message) {
+        
+        if (empty($email)) {
+            // Replace standard PHP Exception if you have a custom Error class
+            throw new Exception;
+        }
         //simulate delay time
         sleep(3);
 
